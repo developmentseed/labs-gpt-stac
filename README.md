518a7a8ca537cab20c0d5cb46e143dce51173eee
Sanjay Bhangar <sanjay@developmentseed.org>
2023-04-10 17:24:33 +0530

    first commit, rough PoC

---
+++ README.md
@@ -0,0 +1,40 @@
+### Implement the ReAct pattern to connect an LLM with a STAC API endpoint
+
+This is inspired by Simon Willison's blog-post: https://til.simonwillison.net/llms/python-react-pattern
+
+The idea here is to develop a natural language interface to a STAC API endpoint, currently the Microsoft Planetary Computer STAC Catalog.
+
+The code is currently very rudimentary and experimental, but already shows promising results.
+
+### How to run
+
+Create an environment variable called OPENAI_API_KEY with your OpenAI API key.
+
+```
+python
+
+from main import query
+
+query("Can you find me satellite images of Mumbai on 7th November, 2014?")
+
+> query("Can you get me satellite imagery for Seattle for 10th December, 2
+   ...: 018?")
+
+Observation: The STAC query returns a list of assets that are available for the given parameters of the bounding box and datetime, which includes imagery from NOAA GOES satellite (GLM-L2-LCFA/2018/345/00) as well as MODIS collection 6.1 (MYD21A2.A2018345.h09v04.061.2021350231530) which has several different assets available, including metadata and various thermal bands. The rendered preview image can be viewed at https://planetarycomputer.microsoft.com/api/data/v1/item/preview.png?collection=modis-21A2-061&item=MYD21A2.A2018345.h09v04.061.2021350231530&assets=LST_Day_1KM&tile_format=png&colormap_name=jet&rescale=255%2C310&format=png
+```
+
+In the above example, ChatGPT constructs queries to Wikipedia, gets the bounding box for Seattle, and uses that to construct a query to the STAC API for the bounding box and datetime requests. It currently only processes the first two results returned, but this can be easily improved.
+
+
+### TODO
+
+This is a very rough quick and dirty PoC. To improve this:
+
+ - Move from wikipedia to using a real geocoder to fetch bounding boxes for a place
+ - Allow it to use more complex STAC search functionality
+ - Format the STAC search result object more appropriately to send back to ChatGPT for it to interpret results.
+ - Augment ChatGPT's natural language answer with all the links, etc. from the actual STAC API response.
+
+
+
+   
---
+++ main.py
@@ -0,0 +1,152 @@
+# This code is Apache 2 licensed:
+# https://www.apache.org/licenses/LICENSE-2.0
+import openai
+import re
+import httpx
+from pystac_client import Client
+import json
+import os
+
+if not 'OPENAI_API_KEY' in os.environ:
+    raise Exception("OPENAI_API_KEY must be defined in your environment")
+
+openai.api_key = os.environ['OPENAI_API_KEY']
+stac_endpoint = "https://planetarycomputer.microsoft.com/api/stac/v1"
+
+class ChatBot:
+    def __init__(self, system=""):
+        self.system = system
+        self.messages = []
+        if self.system:
+            self.messages.append({"role": "system", "content": system})
+    
+    def __call__(self, message):
+        self.messages.append({"role": "user", "content": message})
+        result = self.execute()
+        self.messages.append({"role": "assistant", "content": result})
+        return result
+    
+    def execute(self):
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.messages)
+        # Uncomment this to print out token usage each time, e.g.
+        # {"completion_tokens": 86, "prompt_tokens": 26, "total_tokens": 112}
+        # print(completion.usage)
+        return completion.choices[0].message.content
+
+prompt = """
+You run in a loop of Thought, Action, PAUSE, Observation.
+At the end of the loop you output an Answer
+Use Thought to describe your thoughts about the question you have been asked.
+Use Action to run one of the actions available to you - then return PAUSE.
+Observation will be the result of running those actions.
+
+The questions will involve getting satellite imagery out of a STAC catalog.
+
+To resolve the question, you have the following tools available that you can use.
+
+Your available actions are:
+
+calculate:
+e.g. calculate: 4 * 7 / 3
+Runs a calculation and returns the number - uses Python so be sure to use floating point syntax if necessary
+
+wikipedia:
+e.g. wikipedia: Mumbai
+Returns a summary from searching Wikipedia
+
+stac:
+e.g. stac: bbox=[-73.21, 43.99, -73.12, 44.05] && datetime=['2019-01-01T00:00:00Z', '2019-01-02T00:00:00Z']
+Will query the Microsoft Planetary Computer STAC endpoint for STAC records for that bbox and datetime and return a JSON representation of the item assets returned from the STAC API.
+Please ensure the STAC query is entered exactly as above, with a bbox representing the lat / lng extents of the area to get and ensure there is a space after the comma between elements of the array.
+and the datetime representing timestamps for start and end time to search the catalog within. Always return the rendered preview URL from the items.
+
+Please remember that these are your only three available actions. Do not attempt to put any word after "Action: " other than wikipedia, calculate or stac. THIS IS A HARD RULE.
+
+Always look things up on Wikipedia if you have the opportunity to do so.
+
+Example session:
+
+Question: Can you point me to satellite images for 2019 January, for the capital of France?
+Thought: I should look up France on Wikipedia, find its capital, and find its bounding box extent.
+Action: wikipedia: France
+PAUSE
+
+You will be called again with this:
+
+Observation: France is a country. The capital is Paris. Its bbox is [27, 54, 63, 32.5]
+
+You then output:
+
+Thought: I should now query the STAC catalog to fetch data about satellite images of Paris.
+
+Action: stac: bbox=[27, 54, 63, 32.5] && datetime=['2019-01-01T00:00:00Z', '2019-01-02T00:00:00Z']
+PAUSE
+
+You will be called again with the output from the STAC query as JSON. Use that to give the user information about what is available on STAC for their query.
+
+""".strip()
+
+
+action_re = re.compile('^Action: (\w+): (.*)$')
+
+def query(question, max_turns=5):
+    i = 0
+    bot = ChatBot(prompt)
+    next_prompt = question
+    while i < max_turns:
+        i += 1
+        result = bot(next_prompt)
+        print(result)
+        actions = [action_re.match(a) for a in result.split('\n') if action_re.match(a)]
+        if actions:
+            # There is an action to run
+            action, action_input = actions[0].groups()
+            if action not in known_actions:
+                raise Exception("Unknown action: {}: {}".format(action, action_input))
+            print(" -- running {} {}".format(action, action_input))
+            observation = known_actions[action](action_input)
+            print("Observation:", observation)
+            next_prompt = "Observation: {}".format(observation)
+        else:
+            return
+
+
+def stac(q):
+    bbox_regex = r"bbox=\[(.*?)\]"
+    datetime_regex = r"datetime=\[(.*?)\]"
+
+    # find the bbox and datetime values using regular expressions
+    bbox_match = re.search(bbox_regex, q)
+    datetime_match = re.search(datetime_regex, q)
+
+    # split the bbox and datetime values into arrays
+    bbox = bbox_match.group(1).split(", ")
+    datetime = datetime_match.group(1).split(", ")
+    datetime = [d.strip().strip("'") for d in datetime]
+    print('datetime', datetime)
+    api = Client.open('https://planetarycomputer.microsoft.com/api/stac/v1')
+
+    results = api.search(
+        max_items=5,
+        bbox=bbox,
+        datetime=datetime,
+    )
+    items = [i.get_assets() for i in results.items()]
+    return items[0:2]
+
+def wikipedia(q):
+    return httpx.get("https://en.wikipedia.org/w/api.php", params={
+        "action": "query",
+        "list": "search",
+        "srsearch": q,
+        "format": "json"
+    }).json()["query"]["search"][0]["snippet"]
+
+def calculate(what):
+    return eval(what)
+
+known_actions = {
+    "wikipedia": wikipedia,
+    "calculate": calculate,
+    "stac": stac
+}
